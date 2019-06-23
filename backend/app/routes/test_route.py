1e50279af570d08c7786bd24e5aa8e57ea8195ef
JHowell45 <jhowell45@googlemail.com>
2019-06-23 01:02:04 +0100

    Create test endpoint.

--- backend/app/routes/test_route.py
+++ backend/app/routes/test_route.py
@@ -1,3 +1,12 @@
 """
 
 """
+from flask_restplus import Resource
+
+from app.extensions import api
+
+
+@api.route("/test")
+class TestEndpointRoute(Resource):
+    def get(self):
+        return {"test": "all good!"}
