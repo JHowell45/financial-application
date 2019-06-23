e1e0945fe8f773ef2b1bcc3c36ef0fb3668772db
JHowell45 <jhowell45@googlemail.com>
2019-06-23 01:45:05 +0100

    abstracting extensions.

--- backend/app/extensions/__init__.py
+++ backend/app/extensions/__init__.py
@@ -3,9 +3,4 @@
 This file contains the instances of all of the extensions required for this Flask
 application.
 """
-from flask_restplus import Api
-
-from app.routes.test_route import api as test_ns
-
-api = Api()
-api.add_namespace(test_ns)
+from .restplus_extension import api
