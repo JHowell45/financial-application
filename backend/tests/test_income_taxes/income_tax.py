f2a532d9e8e803816f3385744f8942f8785640b7
JHowell45 <jhowell45@googlemail.com>
2019-06-25 00:00:33 +0100

    Updating tests.

--- backend/tests/test_income_taxes/income_tax.py
+++ backend/tests/test_income_taxes/income_tax.py
@@ -7,7 +7,7 @@ from app.income_taxes.income_tax import calculate_income_tax
 
 
 class TestIncomeTax:
-    income_test_results = [(12500, 0)]
+    income_test_results = [(12500, 0), (100000, 27496)]
 
     @pytest.parametrize("income,income_tax", income_test_results)
     def test_result(self, income, income_tax):
