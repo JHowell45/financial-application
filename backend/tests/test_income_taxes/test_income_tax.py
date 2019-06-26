13104b7ef52c690dd8951c1ea6d03d2301f73052
JHowell45 <jhowell45@googlemail.com>
2019-06-26 22:12:00 +0100

    Updated and fixed test.

--- backend/tests/test_income_taxes/test_income_tax.py
+++ backend/tests/test_income_taxes/test_income_tax.py
@@ -7,8 +7,14 @@ from app.income_taxes.income_tax import calculate_income_tax
 
 
 class TestIncomeTax:
-    income_test_results = [(12500, 0), (100000, 27496)]
+    income_test_results = [
+        (10000, 0),
+        (25000, 2498.20),
+        (50000, 7498.20),
+        (100000, 27496.4),
+        (500000, 204370.95),
+    ]
 
-    @pytest.parametrize("income,income_tax", income_test_results)
+    @pytest.mark.parametrize("income,income_tax", income_test_results)
     def test_result(self, income, income_tax):
         assert calculate_income_tax(income) == income_tax
