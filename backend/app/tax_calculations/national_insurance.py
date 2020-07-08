72c5e4999d42fbdf23b1e9ac3068ce8c442a5a3b
JHowell45 <jhowell45@googlemail.com>
2020-07-08 17:16:57 +0100

    Deleted old files, no longer needed as redundant.

--- backend/app/tax_calculations/national_insurance.py
+++
@@ -1,9 +0,0 @@
-"""Use this file for calculating the NI tax.
-
-This file contains the functions for calculating the national insurance for a given
-user's income and returning the national insurance being paid.
-"""
-
-
-def calculate_national_insurance(income: float):
-    monthly_income = income / 12
