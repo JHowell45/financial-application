0ba77efddfbf96b1dcedbdee0278f07dcb2fa012
JHowell45 <jhowell45@googlemail.com>
2020-07-08 17:20:43 +0100

    Added tests for national insurance calculations.

--- backend/tests/test_tax_calculations/test_income_tax.py
+++ backend/tests/test_tax_calculations/test_income_tax.py
@@ -3,7 +3,7 @@
 """
 import pytest
 
-from app.tax_calculations import calculate_income_tax
+from app.tax_calculations import calculate_income_tax, calculate_national_insurance
 
 
 class TestIncomeTax:
@@ -18,3 +18,17 @@ class TestIncomeTax:
     @pytest.mark.parametrize("income,income_tax", income_test_results)
     def test_result(self, income, income_tax):
         assert calculate_income_tax(income) == income_tax
+
+
+class TestNationalInsurance:
+    income_test_results = [
+        (10000, 60),
+        (25000, 1860),
+        (50000, 4860),
+        (100000, 5860),
+        (500000, 13860),
+    ]
+
+    @pytest.mark.parametrize("income,national_insurance", income_test_results)
+    def test_result(self, income, national_insurance):
+        assert calculate_national_insurance(income) == national_insurance
